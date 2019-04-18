3e1c4761c889a10e492fef7eae711f85b23970e6
yanagi-ori <yanagiori@gmail.com>
2019-04-18 10:36:22 +0400

    config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+token = '885275180:AAGDMJcATEbqQWwIiKb-zpAwvR8hkM-N3xs'  # Токен бота
+database_name = 'data.db'  # Файл с базой данных
+shelve_name = 'shelve.db'  # Файл с хранилищем
