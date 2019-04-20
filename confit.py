2cce8b813550c61b93a9a0b66dff51cd3e04b1a3
Ivan Bogatyrev <45294855+yanagi-ori@users.noreply.github.com>
2019-04-20 14:39:02 +0400

    Create confit.py

---
+++ confit.py
@@ -0,0 +1,5 @@
+token = 'token'
+database_name = 'data.db'  # Файл с базой данных
+admins = {569763743}
+start_keyboard = [['Work', 'Rest'], ['Send Feedback', 'Settings']]
+return_keyboard = [['Work', 'Rest'], ['Send Feedback', 'Settings']]
