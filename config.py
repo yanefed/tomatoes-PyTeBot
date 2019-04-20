74a385a845697fb7c7ee920ea71c1b7920ae6eae
yanagi-ori <yanagiori@gmail.com>
2019-04-20 14:36:03 +0400

    deleted token

--- config.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-token = 'token'  
-database_name = 'data.db'  # Файл с базой данных
-admins = {569763743}
-start_keyboard = [['Work', 'Rest'], ['Send Feedback', 'Settings']]
-return_keyboard = [['Work', 'Rest'], ['Send Feedback', 'Settings']]
